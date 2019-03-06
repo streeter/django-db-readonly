720da5f1564b9aa0c34e219be7b1fe8acad9e011
garciparedes <sergio@garciparedes.me>
2019-03-06 17:45:02 +0100

    Allowed to modify "read_only" and "readonly_dbs" attributes.

--- readonly/__init__.py
+++ readonly/__init__.py
@@ -9,6 +9,7 @@ from time import time
 
 import django
 from django.conf import settings
+
 if django.VERSION < (1, 7):
     from django.db.backends import util
 else:
@@ -16,7 +17,6 @@ else:
 from logging import getLogger
 
 from readonly.exceptions import DatabaseWriteDenied
-
 
 logger = getLogger('django.db.backends')
 
@@ -58,11 +58,17 @@ class ReadOnlyCursorWrapper(object):
         'INSERT INTO', 'UPDATE', 'REPLACE', 'DELETE FROM',
     )
 
-    def __init__(self, cursor, db):
+    def __init__(self, cursor, db, read_only=None, readonly_dbs=None):
+
+        if read_only is None:
+            read_only = _readonly()
+        if readonly_dbs is None:
+            readonly_dbs = _get_readonly_dbs()
+
         self.cursor = cursor
         self.db = db
-        self.readonly = _readonly()
-        self.readonly_dbs = _get_readonly_dbs()
+        self.readonly = read_only
+        self.readonly_dbs = readonly_dbs
 
     def execute(self, sql, params=()):
         # Check the SQL
@@ -89,12 +95,13 @@ class ReadOnlyCursorWrapper(object):
 
     def _write_to_readonly_db(self):
         return (
-            not self.readonly_dbs
-            or self.db.settings_dict['NAME'] in self.readonly_dbs)
+                not self.readonly_dbs
+                or self.db.settings_dict['NAME'] in self.readonly_dbs)
 
     @property
     def _last_executed(self):
         return getattr(self.cursor, '_last_executed', '')
+
 
 class CursorWrapper(util.CursorWrapper):
     def __init__(self, cursor, db):
@@ -140,6 +147,7 @@ class CursorDebugWrapper(CursorWrapper):
                 duration, sql, param_list,
                 extra={'duration': duration, 'sql': sql, 'params': param_list}
             )
+
 
 if _readonly():
     # Monkey Patching!
